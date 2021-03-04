10810ef26229d50a3e1f79b5318aec718c8af77b
Seden Nur Çakmak <seden.cakmak@mf.karaelmas.edu.tr>
2021-03-04 20:41:49 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Sayın " + customer.FirstName + " " + customer.LastName + " Tebrikler Kaydınız Oluşturuldu. ");
+        }
+
+        public void Add2(Customer customer)
+        {
+            Console.WriteLine(customer.Id + "." + customer.FirstName + " " + customer.LastName + ", "
+                + customer.Age + ", " + customer.Email + " ");
+        }
+        public void Update(Customer customer)
+        {
+            Console.WriteLine("Sayın " + customer.FirstName + " " + customer.LastName + " Müşteri Bilgileri Güncellendi.");
+        }
+
+        public void Deletion(Customer customer)
+        {
+            Console.WriteLine("Sayın " + customer.FirstName + " " + customer.LastName + " Kaydınız Silinmiştir.");
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 0001;
+            customer1.FirstName = "Seden";
+            customer1.LastName = "Çakmak";
+            customer1.Email = "seden@gmail.com";
+            customer1.Age = 20;
+
+            Customer customer2 = new Customer();
+            customer2.Id = 0002;
+            customer2.FirstName = "Elif";
+            customer2.LastName = "Sertdemir";
+            customer2.Email = "sertdemir@gmail.com";
+            customer2.Age = 21;
+
+            Customer customer3 = new Customer();
+            customer3.Id = 0003;
+            customer3.FirstName = "Burak";
+            customer3.LastName = "Ateş";
+            customer3.Email = "tesekkurlerburakates@gmail.com";
+            customer3.Age = 22;
+
+            Customer customer4 = new Customer();
+            customer4.Id = 0004;
+            customer4.FirstName = "Emre";
+            customer4.LastName = "Kahveci";
+            customer4.Email = "kahveciemre@gmail.com";
+            customer4.Age = 34;
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Id + "." + customer.FirstName + "  " + customer.LastName + " , " + customer.Age + " , " + customer.Email);
+
+            }
+            Console.WriteLine("Müşteriler Listelendi.");
+            Console.WriteLine("---------------------------------");
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer4);
+            customerManager.Add2(customer4);
+            Console.WriteLine("----------------------------------");
+
+            customerManager.Update(customer4);
+            customerManager.Deletion(customer4);
+        }
+    }
+}
